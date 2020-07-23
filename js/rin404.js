94ce6e63443717e5bb1abde09be630e8a278f845
澤村・Eriri々 <38132731+Rintarou416@users.noreply.github.com>
2020-07-23 19:29:35 +0800

    Update rin404.js

--- js/rin404.js
+++ js/rin404.js
@@ -1,7 +1,7 @@
 //Copyright Rintarou°
 // console版权输出
 console.log("%c Rintarou°", "color:#696969;font-size:5em;");
-console.log("%c ", "background: url(//cdn.jsdelivr.net/gh/Rintarou416/blog-cdn/js/console.png) no-repeat center;padding-left:880px;padding-bottom: 220px;")
+console.log("%c ", "background: url(https://cdn.jsdelivr.net/gh/Rintarou416/blog-cdn/js/console.png) no-repeat center;padding-left:880px;padding-bottom: 220px;")
 console.log("%c 失敗（しっぱい）は終（お）わりではない、諦（あきら）めるこそが終わりである。 \n ゴールが見えないから人生（じんせい）はおもしろい。\n 失败并非结束，放弃才是完结。\n 正由于看不到终点，人生才有趣。 \n 2018-10-28","color:#7266ba;background-color:#8fffffb5;padding:10px;");
 // 评论打卡
 function addNumber(a) {
